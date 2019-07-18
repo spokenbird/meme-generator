7574bfb9c7b8440171a7be745f188f7525158311
spokenbird <spokenbird@gmail.com>
2019-07-17 23:33:26 -0700

    Added input to text and Image Load
    Input elements now push text onto image and image load button loads image from image URL input.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,37 @@
-let topTextInput, bottomTextInput, imageInput, loadImageBtn, imageURL, createBtn;
-
 // Sets the Load Image Button
-loadImageBtn = document.querySelector('load-image');
+let loadImageBtn = document.querySelector('.load-image');
 
 // Event listener to trigger Load Image Button
-loadImageBtn.addEventListener('click', function(evt) {
-    // Get image url from input
-    imageURL = document.querySelector('image-url').value;
-    // Create img element
-    let img = document.createElement('img');
-    // Set src attribute
-    
+loadImageBtn.addEventListener('click', function (evt) {
+  // Prevents page from reloading
+  evt.preventDefault()
+  // Get image url from input
+  let imageURL = document.querySelector('.image-url').value;
+  // Variable to set an existing img
+  let existingImg = document.querySelector('#preview-img');
+  // Set src to input value
+  existingImg.src = imageURL;
+  });
 
-});
+// Set text input variables for collecting user input
+const topTextInput = document.querySelector('#top-text-input');
+const bottomTextInput = document.querySelector('#bottom-text-input');
+
+
+// Set text div variables
+const topText = document.querySelector('.top-text');
+const bottomText = document.querySelector('.bottom-text');
+
+// Send user input text to corresponding text-input div
+topTextInput.oninput = () => {
+  console.log(topTextInput);
+  topText.innerHTML = topTextInput.value;
+}
+bottomTextInput.oninput = () => {
+  bottomText.textContent = bottomTextInput.value;
+  console.log(`The value of the bottom text input is currently ${bottomTextInput.value}`)
+}
 
 
 
-
+// MAKE MEME BUTTON --> Copy the image div to the lower section of the page
